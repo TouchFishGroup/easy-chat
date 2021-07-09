e04708874c07f333db491915e529fc6be0116d3b
代杳杳 <8527512+pepsi77@user.noreply.gitee.com>
2021-07-09 18:25:26 +0800

    add Vant

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div id="easy-chat" class="bg-yellow-200 flex container mx-auto my-24 w-3/4 h-2/3 overflow-hidden">
-    <div class="bg-pink-400 h-full w-1/6 flex flex-col justify-between">
-      <div class="block border-2 rounded-full h-10 w-10 cursor-pointer">头像</div>
-      <router-link to="/" class="block border-2 rounded-full h-10 w-10 cursor-pointer">聊天列表</router-link>
-      <router-link to="/friend" class="block border-2 rounded-full h-10 w-10 cursor-pointer">通讯录</router-link>
+  <div id="easy-chat"
+       class="bg-yellow-200 flex container mx-auto my-24  w-3/4 h-2/3">
+    <div class="bg-pink-400 h-full w-1/6  container flex flex-col justify-around">
+    
+    <van-image
+  round
+  fit="cover"
+  class="h-10 w-10 mx-auto cursor-pointer"
+  src="https://img.yzcdn.cn/vant/cat.jpeg"
+/>
+      <router-link to="/"
+                   class="block border-2 rounded-full h-10 w-10 mx-auto  cursor-pointer">聊天列表</router-link>
+      <router-link to="/friend"
+                   class="block border-2 rounded-full h-10 w-10 mx-auto cursor-pointer">通讯录</router-link>
     </div>
     <div class="bg-red-600 h-full w-1/3">
       <input>
@@ -14,16 +23,17 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App'
-})
+  name: "App",
+});
 </script>
 <style >
-  body,html {
-    height: 100%;
-    width: 100%;
-    overflow: hidden;
-    }
+body,
+html {
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Image as VanImage } from 'vant'//头像图片
+import { Button } from 'vant';
 import './index.css'
 const app = createApp(App)
 
 app.use(router)
+app.use(VanImage)
+app.use(Button)
 app.mount('#app')
